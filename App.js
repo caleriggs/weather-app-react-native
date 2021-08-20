7deed11c72c9006e09810c13ff9f7be467787074
Cale Riggs <riggs.cale@gmail.com>
2021-08-19 20:26:44 -0500

    removed unused imports

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import CurrentWeather from './src/Components/CurrentWeather';
